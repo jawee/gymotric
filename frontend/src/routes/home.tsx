ee7412d51c466f155d4ea330c3977a179f760468
Andreas Olsson <olsson.andreas@gmail.com>
2024-12-23 11:03:55 +0100

    cleanup

--- frontend/src/routes/home.tsx
+++ frontend/src/routes/home.tsx
@@ -2,20 +2,10 @@ import Menu from '../components/menu';
 import WorkoutsList from '../components/workouts-list';
 
 const Home = () => {
-    // const [count, setCount] = useState(0)
-
-    // const fetchData = () => {
-    //   fetch('http://localhost:8080/')
-    //     .then(response => response.text())
-    //     .then(data => setMessage(data))
-    //     .catch(error => console.error('Error fetching data:', error));
-    // };
-
-    // const [message, setMessage] = useState<string>('');
-
     const newWorkout = () => {
         console.log("new workout");
     };
+
     return (
         <>
             <Menu />
