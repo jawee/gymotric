06f3eb7533439a0cb6042493a02f3a751ccbfa1c
Andreas Olsson <olsson.andreas@gmail.com>
2024-12-30 21:57:09 +0100

    formatting

--- internal/exercisetypes/repository.go
+++ internal/exercisetypes/repository.go
@@ -21,7 +21,7 @@ type ExerciseType struct {
 type ExerciseTypeRepository interface {
 	CreateAndReturnId(context context.Context, exerciseType repository.CreateExerciseTypeAndReturnIdParams) (string, error)
 	DeleteById(context context.Context, exerciseTypeId string) error
-	GetAll(context context.Context) ([]ExerciseType, error) 
+	GetAll(context context.Context) ([]ExerciseType, error)
 }
 
 func (e exerciseTypeRepository) GetAll(context context.Context) ([]ExerciseType, error) {
@@ -38,12 +38,11 @@ func (e exerciseTypeRepository) GetAll(context context.Context) ([]ExerciseType,
 }
 
 func newExerciseType(v repository.ExerciseType) ExerciseType {
-	return ExerciseType {
-		ID: v.ID,
+	return ExerciseType{
+		ID:   v.ID,
 		Name: v.Name,
 	}
 }
-
 
 func (e exerciseTypeRepository) DeleteById(context context.Context, exerciseTypeId string) error {
 	return e.repo.DeleteExerciseTypeById(context, exerciseTypeId)
