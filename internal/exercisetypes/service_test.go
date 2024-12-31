d28f0997ac15dd528b6ed71363df8aca0ece77e7
Andreas Olsson <olsson.andreas@gmail.com>
2024-12-31 09:30:03 +0100

    tests

--- internal/exercisetypes/service_test.go
+++ internal/exercisetypes/service_test.go
@@ -6,26 +6,32 @@ import (
 	"weight-tracker/internal/repository"
 )
 
-// type Service interface {
-// 	GetAll(context context.Context) ([]ExerciseType, error)
-// 	DeleteById(context context.Context, exerciseTypeId string) error
-// 	CreateAndReturnId(context context.Context, exerciseType createExerciseTypeRequest) (string, error)
-// }
-
 type repoMock struct {
-	exerciseTypes []ExerciseType
+	ExerciseTypes []ExerciseType
 }
 
-func (r repoMock) CreateAndReturnId(context context.Context, exerciseType repository.CreateExerciseTypeAndReturnIdParams) (string, error) {
-	panic("unimplemented")
+func (r *repoMock) CreateAndReturnId(context context.Context, exerciseType repository.CreateExerciseTypeAndReturnIdParams) (string, error) {
+	exerciseTypeStruct := ExerciseType{
+		ID: exerciseType.ID,
+		Name: exerciseType.Name,
+	}
+	r.ExerciseTypes = append(r.ExerciseTypes, exerciseTypeStruct)
+
+	return exerciseType.ID, nil
 }
 
-func (r repoMock) DeleteById(context context.Context, exerciseTypeId string) error {
-	panic("unimplemented")
+func (r *repoMock) DeleteById(context context.Context, exerciseTypeId string) error {
+	for i, v := range r.ExerciseTypes {
+		if v.ID == exerciseTypeId {
+			r.ExerciseTypes = append(r.ExerciseTypes[:i], r.ExerciseTypes[i+1:]...)
+		}
+	}
+
+	return nil
 }
 
-func (r repoMock) GetAll(context context.Context) ([]ExerciseType, error) {
-	return r.exerciseTypes, nil
+func (r *repoMock) GetAll(context context.Context) ([]ExerciseType, error) {
+	return r.ExerciseTypes, nil
 }
 
 func TestGetAll(t *testing.T) {
@@ -33,7 +39,7 @@ func TestGetAll(t *testing.T) {
 		{ ID: "b", Name: "b" },
 		{ ID: "a", Name: "a" },
 	}}
-	service := NewService(repoMock)
+	service := NewService(&repoMock)
 
 	result, err := service.GetAll(context.Background())
 	if err != nil {
@@ -46,5 +52,41 @@ func TestGetAll(t *testing.T) {
 
 	if result[0].Name != "a" {
 		t.Fatalf("Not sorted\n")
+	}
+}
+
+func TestDeleteById(t *testing.T) {
+	repoMock := repoMock{[]ExerciseType{
+		{ ID: "b", Name: "b" },
+		{ ID: "a", Name: "a" },
+	}}
+
+	service := NewService(&repoMock)
+	err := service.DeleteById(context.Background(), "a")
+
+	if err != nil {
+		t.Fatalf("Got err %s\n", err)
+	}
+
+	if len(repoMock.ExerciseTypes) != 1 {
+		t.Fatalf("Expected repository to only have 1 exercise type, but has %d\n", len(repoMock.ExerciseTypes))
+	}
+}
+
+func TestCreateAndReturnId(t *testing.T) {
+	repoMock := repoMock{[]ExerciseType{
+	}}
+
+	service := NewService(&repoMock)
+	_, err := service.CreateAndReturnId(context.Background(), createExerciseTypeRequest{
+		Name: "a",
+	})
+
+	if err != nil {
+		t.Fatalf("Got err %s\n", err)
+	}
+
+	if len(repoMock.ExerciseTypes) != 1 {
+		t.Fatalf("Expected repository to only have 1 exercise type, but has %d\n", len(repoMock.ExerciseTypes))
 	}
 }
