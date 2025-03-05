e14e668b0ad73bbebf00d03702bdb6d2abc15115
Andreas Olsson <olsson.andreas@gmail.com>
2025-03-05 19:59:53 +0100

    Table workouts

--- frontend/src/components/workouts-list.tsx
+++ frontend/src/components/workouts-list.tsx
@@ -1,73 +1,100 @@
 import { Input } from "@/components/ui/input";
 import { useEffect, useId, useState } from "react";
-import { Link, useNavigate } from "react-router";
+import { useNavigate } from "react-router";
 import { Workout } from "../models/workout";
 import ApiService from "../services/api-service";
 import { Button } from "./ui/button";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table"
 
 const CreateWorkoutForm = () => {
-    const [name, setName] = useState<string>("");
-    const nameId = useId()
+  const [name, setName] = useState<string>("");
+  const nameId = useId()
 
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    const createWorkout = async (event: React.FormEvent<HTMLFormElement>) => {
-        event.preventDefault();
-        const res = await ApiService.createWorkout(name);
+  const createWorkout = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const res = await ApiService.createWorkout(name);
 
-        if (res.status !== 201) {
-            console.log("Error");
-            return
-        }
+    if (res.status !== 201) {
+      console.log("Error");
+      return
+    }
 
-        const response = await res.json()
+    const response = await res.json()
 
-        navigate("/app/workouts/" + response.id);
-    };
+    navigate("/app/workouts/" + response.id);
+  };
 
-    return (
-        <>
-            <form onSubmit={createWorkout}>
-                <Input id={nameId} value={name} onChange={e => setName(e.target.value)} type="text" placeholder="Name of workout" />
-                <Button type="submit">Create workout</Button>
-            </form>
-        </>
-    );
+  return (
+    <>
+      <form className="mt-5" onSubmit={createWorkout}>
+        <Input id={nameId} value={name} onChange={e => setName(e.target.value)} type="text" placeholder="Name of workout" />
+        <Button type="submit">Create workout</Button>
+      </form>
+    </>
+  );
 };
 
 const WorkoutsList = () => {
-    const [workouts, setWorkouts] = useState<Workout[]>([]);
-    const [isCreateWorkoutMode, setIsCreateWorkoutMode] = useState<boolean>(false);
+  const [workouts, setWorkouts] = useState<Workout[]>([]);
+  const [isCreateWorkoutMode, setIsCreateWorkoutMode] = useState<boolean>(false);
 
-    const addWorkout = () => {
-        setIsCreateWorkoutMode(true);
+  const navigate = useNavigate();
+
+  const addWorkout = () => {
+    setIsCreateWorkoutMode(true);
+  };
+
+  useEffect(() => {
+    const fetchWorkouts = async () => {
+      const res = await ApiService.fetchWorkouts();
+
+      if (res.status === 200) {
+        const resObj = await res.json();
+        setWorkouts(resObj.workouts);
+      }
     };
-    useEffect(() => {
-        const fetchWorkouts = async () => {
-            const res = await ApiService.fetchWorkouts();
 
-            if (res.status === 200) {
-                const resObj = await res.json();
-                setWorkouts(resObj.workouts);
-            }
-        };
+    fetchWorkouts();
+  }, []);
 
-        fetchWorkouts();
-    }, []);
 
-    return (
-        <>
-            <ul>
-                {workouts.map(workout => {
-                    return (
-                        <li key={workout.id}><Link to={"/app/workouts/" + workout.id}>{new Date(workout.created_on).toDateString()}: {workout.name}</Link></li>
-                    )
-                })}
-            </ul>
-            {!isCreateWorkoutMode && <Button onClick={addWorkout}>Create workout</Button>}
-            {isCreateWorkoutMode && <CreateWorkoutForm />}
-        </>
-    );
+  return (
+    <>
+      <Table>
+        <TableHeader>
+          <TableRow>
+            <TableHead>Workout name</TableHead>
+            <TableHead>Date</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          {workouts.map(workout => {
+            return (
+              <TableRow key={workout.id} onClick={() => navigate("/app/workouts/" + workout.id)}>
+                <TableCell className="font-medium">
+                    {workout.name}
+                </TableCell>
+                <TableCell>
+                  {new Date(workout.created_on).toDateString()}
+                </TableCell>
+              </TableRow>
+            )
+          })}
+        </TableBody>
+      </Table>
+      {!isCreateWorkoutMode && <Button className="mt-5" onClick={addWorkout}>Create workout</Button>}
+      {isCreateWorkoutMode && <CreateWorkoutForm />}
+    </>
+  );
 }
 
 export default WorkoutsList;
