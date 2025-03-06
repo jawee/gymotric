0d4aa30553235f2b612ac368102d8a7b8dc0a11b
Andreas Olsson <olsson.andreas@gmail.com>
2025-03-06 05:22:08 +0100

    menu error fix

--- frontend/src/components/menu.tsx
+++ frontend/src/components/menu.tsx
@@ -8,10 +8,9 @@ const Menu = () => {
     return (
         <NavigationMenu>
             <NavigationMenuLink asChild><Link to="/app">Home</Link></NavigationMenuLink>
-            <NavigationMenuLink><Link to="/app/workouts">Workouts</Link></NavigationMenuLink>
-            <NavigationMenuLink><Link to="/app/exercise-types">Exercise types</Link></NavigationMenuLink>
-            <NavigationMenuLink><Link to="/app/logout">Log Out</Link></NavigationMenuLink>
-
+            <NavigationMenuLink asChild><Link to="/app/workouts">Workouts</Link></NavigationMenuLink>
+            <NavigationMenuLink asChild><Link to="/app/exercise-types">Exercise types</Link></NavigationMenuLink>
+            <NavigationMenuLink asChild><Link to="/app/logout">Log Out</Link></NavigationMenuLink>
         </NavigationMenu>
     );
 };
