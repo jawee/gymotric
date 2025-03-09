bab0bab4d1f4745de16e0f45a0629c5d7f60cf71
Andreas Olsson <olsson.andreas@gmail.com>
2025-03-09 16:22:29 +0100

    some styling of home

--- frontend/src/routes/home.tsx
+++ frontend/src/routes/home.tsx
@@ -1,14 +1,13 @@
 import WorkoutsList from '../components/workouts-list';
 
 const Home = () => {
-    return (
-        <>
-            <h1>Home</h1>
-
-            <h3>Workouts</h3>
-            <WorkoutsList />
-        </>
-    );
+  return (
+    <>
+      <h1 className="text-xl mb-2">Home</h1>
+      <h2 className="text-l mb-2">Workouts</h2>
+      <WorkoutsList />
+    </>
+  );
 }
 
 export default Home;
