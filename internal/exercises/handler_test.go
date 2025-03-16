df108c95c728caa0ceceef2bdd8984c7fe6fcb79
Andreas Olsson <olsson.andreas@gmail.com>
2025-03-16 13:01:03 +0100

    createExerciseHandler test

--- internal/exercises/handler_test.go
+++ internal/exercises/handler_test.go
@@ -1,8 +1,10 @@
 package exercises
 
 import (
+	"bytes"
 	"context"
 	"database/sql"
+	"encoding/json"
 	"net/http"
 	"net/http/httptest"
 	"testing"
@@ -106,5 +108,63 @@ func TestGetExercisesByWorkoutIdHandlerNotFound(t *testing.T) {
 	if status := rr.Code; status != http.StatusBadRequest {
 		t.Errorf("handler returned wrong status code: got %v want %v", status, http.StatusBadRequest)
 	}
+
+	serviceMock.AssertExpectations(t)
+}
+
+func TestCreateExerciseHandler(t *testing.T) {
+	userId := "userId"
+	workoutId := "workoutId"
+	exerciseTypeId := "exerciseTypeId"
+
+	reqBodyObj := createExerciseRequest{
+		ExerciseTypeID: exerciseTypeId,
+	}
+
+	reqBody, err := json.Marshal(reqBodyObj)
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	req, err := http.NewRequest("POST", "/workouts/"+workoutId+"/exercises", bytes.NewBuffer(reqBody))
+	req.SetPathValue("id", workoutId)
+
+	ctx := req.Context()
+	ctx = context.WithValue(ctx, "sub", userId)
+	req = req.WithContext(ctx)
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	serviceMock := serviceMock{}
+	serviceMock.On("CreateAndReturnId", ctx, mock.MatchedBy(func (input createExerciseRequest) bool {
+			return true
+		}),  workoutId, userId).
+		Return("abc", nil).
+		Once()
+
+	rr := httptest.NewRecorder()
+	s := handler{service: &serviceMock}
+	handler := http.HandlerFunc(s.createExerciseHandler)
+
+	handler.ServeHTTP(rr, req)
+
+	if status := rr.Code; status != http.StatusCreated {
+		t.Errorf("handler returned wrong status code: got %v want %v", status, http.StatusOK)
+	}
+
+	if ctype := rr.Header().Get("Content-Type"); ctype != "application/json" {
+		t.Errorf("content type header does not match: got %v want %v",
+			ctype, "application/json")
+	}
+
+	expected := `{"id":"abc"}`
+	if rr.Body.String() != expected {
+		t.Errorf("handler returned unexpected body: got %v want %v",
+			rr.Body.String(), expected)
+	}
+
 	serviceMock.AssertExpectations(t)
 }
