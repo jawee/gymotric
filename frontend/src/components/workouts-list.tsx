53de6a7e55db00e54887dca579289648b66dcbcf
Andreas Olsson <olsson.andreas@gmail.com>
2025-03-17 20:06:53 +0100

    try fix iOS issue with selecing exercise type (#21)

--- frontend/src/components/workouts-list.tsx
+++ frontend/src/components/workouts-list.tsx
@@ -71,7 +71,7 @@ const WorkoutListItem = ({ workout }: WorkoutListItemProps) => {
     };
 
     fetchExercises();
-  }, [exercises]);
+  }, []);
 
   return (
     <div onClick={() => navigate("/app/workouts/" + workout.id)} className="cursor-pointer p-4 border border-gray-200">
