caef01eda4c025b6ee0c0b0ca8e28dd30a41bdbb
Andreas Olsson <olsson.andreas@gmail.com>
2025-03-22 14:22:24 +0100

    styling

--- frontend/src/routes/landing.tsx
+++ frontend/src/routes/landing.tsx
@@ -1,13 +1,21 @@
+import { Button } from "@/components/ui/button";
+import { Dumbbell } from "lucide-react";
 import { Link } from "react-router";
 
 const Landing = () => {
-    return (
-        <>
-            <h1>Welcome</h1>
-            <p>Landing page</p>
-            <Link to="/login">Log In</Link>
-        </>
-    );
+  return (
+    <div className="flex min-h-svh flex-col items-center gap-6 bg-muted p-6 md:p-10">
+      <div className="flex w-full max-w-sm flex-col gap-6">
+        <a href="#" className="flex items-center gap-2 self-center font-medium text-2xl">
+          <div className="flex h-10 w-10 items-center justify-center rounded-md text-primary m-1 ml-0 border-2 border-primary">
+            <Dumbbell className="size-6" />
+          </div>
+          Gymotric
+        </a>
+        <Button><Link to="/login">Log In</Link></Button>
+      </div>
+    </div>
+  );
 }
 
 export default Landing;
