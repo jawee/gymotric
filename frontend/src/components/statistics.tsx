2d8d7ebbad7bff534e03ea6f21d76f5ad028fa0b
Andreas Olsson <olsson.andreas@gmail.com>
2025-03-22 16:13:53 +0100

    Basic statistics

---
+++ frontend/src/components/statistics.tsx
@@ -0,0 +1,36 @@
+import { Statistics } from "../models/statistics";
+import { useEffect, useState } from "react";
+import Loading from "./loading";
+import ApiService from "@/services/api-service";
+
+const StatisticsComponent = () => {
+  const [stats, setStats] = useState<Statistics | null>(null);
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+
+  useEffect(() => {
+    const fetchStats = async () => {
+      const res = await ApiService.fetchStatistics();
+
+      if (res.status === 200) {
+        const resObj = await res.json();
+        setStats(resObj);
+        setIsLoading(false);
+      }
+    };
+
+    fetchStats();
+  }, []);
+
+  if (isLoading || !stats) {
+    return <Loading />;
+  }
+  return (
+    <div>
+      <p>Workouts this week: {stats.week}</p>
+      <p>Workouts this month: {stats.month}</p>
+      <p>Workouts this year: {stats.year}</p>
+    </div>
+  );
+};
+
+export default StatisticsComponent;
