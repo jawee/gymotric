fcadc69ac6c7c140d3db2481220b6d3816a8e0e9
Andreas Olsson <olsson.andreas@gmail.com>
2025-03-29 19:26:45 +0100

    some margin on menu

--- frontend/src/components/menu.tsx
+++ frontend/src/components/menu.tsx
@@ -7,7 +7,7 @@ import { Dumbbell } from "lucide-react";
 
 const Menu = () => {
   return (
-    <div className="flex">
+    <div className="flex mb-4">
       <NavigationMenu className="w-full justify-evenly max-w-none text-center md:max-w-xs">
         <div className="flex h-10 w-14 items-center justify-center rounded-none text-primary m-1 ml-0 border-r-2 border-primary">
           <Dumbbell className="size-6" />
