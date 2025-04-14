d3a27862801ec728200ce40761cd73eb31ea1e29
Andreas Olsson <olsson.andreas@gmail.com>
2025-04-14 20:31:40 +0200

    test deleteExerciseTypeHandler

--- internal/exercisetypes/handler_test.go
+++ internal/exercisetypes/handler_test.go
@@ -209,3 +209,67 @@ func TestGetAllExerciseTypesHandlerErr(t *testing.T) {
 
 	serviceMock.AssertExpectations(t)
 }
+
+func TestDeleteExerciseTypeByIdHandler(t *testing.T) {
+	userId := "userId"
+	exerciseTypeId := "exerciseTypeId"
+
+	req, err := http.NewRequest("DELETE", "/exercise-types/"+exerciseTypeId, nil)
+	req.SetPathValue("id", exerciseTypeId)
+
+	req = populateContextWithSub(req, userId)
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	serviceMock := serviceMock{}
+	serviceMock.On("DeleteById", req.Context(), exerciseTypeId, userId).
+		Return(nil).
+		Once()
+
+	rr := httptest.NewRecorder()
+	s := handler{service: &serviceMock}
+	handler := http.HandlerFunc(s.deleteExerciseTypeByIdHandler)
+	handler.ServeHTTP(rr, req)
+	if status := rr.Code; status != http.StatusNoContent {
+		t.Errorf("handler returned wrong status code: got %v want %v", status, http.StatusOK)
+	}
+
+	serviceMock.AssertExpectations(t)
+}
+
+func TestDeleteExerciseTypeByIdHandlerErr(t *testing.T) {
+	userId := "userId"
+	exerciseTypeId := "exerciseTypeId"
+
+	req, err := http.NewRequest("DELETE", "/exercise-types/"+exerciseTypeId, nil)
+	req.SetPathValue("id", exerciseTypeId)
+
+	req = populateContextWithSub(req, userId)
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	serviceMock := serviceMock{}
+	serviceMock.On("DeleteById", req.Context(), exerciseTypeId, userId).
+		Return(errors.New("Failed")).
+		Once()
+
+	rr := httptest.NewRecorder()
+	s := handler{service: &serviceMock}
+	handler := http.HandlerFunc(s.deleteExerciseTypeByIdHandler)
+	handler.ServeHTTP(rr, req)
+	if status := rr.Code; status != http.StatusBadRequest {
+		t.Errorf("handler returned wrong status code: got %v want %v", status, http.StatusOK)
+	}
+
+	expected := "Failed to delete exercise type\n"
+	if rr.Body.String() != expected {
+		t.Errorf("handler returned unexpected body: got '%v' want '%v'",
+			rr.Body.String(), expected)
+	}
+
+	serviceMock.AssertExpectations(t)
+}
