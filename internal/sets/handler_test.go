a123cacc6b1cc6b14c8cb4377729f36432878993
Andreas Olsson <olsson.andreas@gmail.com>
2025-04-16 19:46:52 +0200

    sets handler tests

--- internal/sets/handler_test.go
+++ internal/sets/handler_test.go
@@ -1,7 +1,9 @@
 package sets
 
 import (
+	"bytes"
 	"context"
+	"encoding/json"
 	"errors"
 	"net/http"
 	"net/http/httptest"
@@ -97,6 +99,204 @@ func TestDeleteSetByIdHandlerErr(t *testing.T) {
 	}
 
 	expected := "Failed to delete set\n"
+	if rr.Body.String() != expected {
+		t.Errorf("handler returned unexpected body: got %v want %v", rr.Body.String(), expected)
+	}
+
+	serviceMock.AssertExpectations(t)
+}
+
+func TestGetSetsByExerciseIdHandler(t *testing.T) {
+	userId := "userId"
+	workoutId := "workoutId"
+	exerciseId := "exerciseId"
+
+	req, err := http.NewRequest("GET", "/workouts/"+workoutId+"/exercises/"+exerciseId+"/sets", nil)
+	req.SetPathValue("id", workoutId)
+	req.SetPathValue("exerciseId", exerciseId)
+
+	req = populateContextWithSub(req, userId)
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	serviceMock := serviceMock{}
+	serviceMock.On("GetByExerciseId", req.Context(), exerciseId, userId).
+		Return([]Set{{ID: "a", Repetitions: 10, Weight: 100, ExerciseID: exerciseId}}, nil).
+		Once()
+
+	rr := httptest.NewRecorder()
+	s := handler{service: &serviceMock}
+	handler := http.HandlerFunc(s.getSetsByExerciseIdHandler)
+	handler.ServeHTTP(rr, req)
+	if status := rr.Code; status != http.StatusOK {
+		t.Errorf("handler returned wrong status code: got %v want %v", status, http.StatusOK)
+	}
+
+	expected := `{"sets":[{"id":"a","repetitions":10,"weight":100,"exercise_id":"exerciseId"}]}`
+	if rr.Body.String() != expected {
+		t.Errorf("handler returned unexpected body: got %v want %v", rr.Body.String(), expected)
+	}
+
+	serviceMock.AssertExpectations(t)
+}
+
+func TestGetSetsByExerciseIdHandlerNoExerciseId(t *testing.T) {
+	userId := "userId"
+	workoutId := "workoutId"
+
+	req, err := http.NewRequest("GET", "/workouts/"+workoutId+"/exercises/sets", nil)
+	req.SetPathValue("id", workoutId)
+
+	req = populateContextWithSub(req, userId)
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	serviceMock := serviceMock{}
+
+	rr := httptest.NewRecorder()
+	s := handler{service: &serviceMock}
+	handler := http.HandlerFunc(s.getSetsByExerciseIdHandler)
+	handler.ServeHTTP(rr, req)
+	if status := rr.Code; status != http.StatusBadRequest {
+		t.Errorf("handler returned wrong status code: got %v want %v", status, http.StatusOK)
+	}
+	expected := "Failed to get sets\n"
+	if rr.Body.String() != expected {
+		t.Errorf("handler returned unexpected body: got %v want %v", rr.Body.String(), expected)
+	}
+}
+
+func TestGetSetsByExerciseIdHandlerServiceErr(t *testing.T) {
+	userId := "userId"
+	workoutId := "workoutId"
+	exerciseId := "exerciseId"
+
+	req, err := http.NewRequest("GET", "/workouts/"+workoutId+"/exercises/"+exerciseId+"/sets", nil)
+	req.SetPathValue("id", workoutId)
+	req.SetPathValue("exerciseId", exerciseId)
+
+	req = populateContextWithSub(req, userId)
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	serviceMock := serviceMock{}
+	serviceMock.On("GetByExerciseId", req.Context(), exerciseId, userId).
+		Return([]Set{}, errors.New("Failed")).
+		Once()
+
+	rr := httptest.NewRecorder()
+	s := handler{service: &serviceMock}
+	handler := http.HandlerFunc(s.getSetsByExerciseIdHandler)
+	handler.ServeHTTP(rr, req)
+	if status := rr.Code; status != http.StatusBadRequest {
+		t.Errorf("handler returned wrong status code: got %v want %v", status, http.StatusOK)
+	}
+
+	expected := "Failed to get sets\n"
+	if rr.Body.String() != expected {
+		t.Errorf("handler returned unexpected body: got %v want %v", rr.Body.String(), expected)
+	}
+
+	serviceMock.AssertExpectations(t)
+}
+
+func TestCreateSetHandler(t *testing.T) {
+	userId := "userId"
+	workoutId := "workoutId"
+	exerciseId := "exerciseId"
+
+	reqBodyObj := createSetRequest{
+		Repetitions: 10,
+		Weight:      100,
+	}
+
+	reqBody, err := json.Marshal(reqBodyObj)
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	req, err := http.NewRequest("POST", "/workouts/"+workoutId+"/exercises/"+exerciseId+"/sets", bytes.NewBuffer(reqBody))
+	req.SetPathValue("id", workoutId)
+	req.SetPathValue("exerciseId", exerciseId)
+
+	req = populateContextWithSub(req, userId)
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	serviceMock := serviceMock{}
+	serviceMock.On("CreateAndReturnId", req.Context(), mock.MatchedBy(func(input createSetRequest) bool {
+		return input.Repetitions == 10 && input.Weight == 100
+	}), exerciseId, userId).
+		Return("setId", nil).
+		Once()
+
+	rr := httptest.NewRecorder()
+	s := handler{service: &serviceMock}
+	handler := http.HandlerFunc(s.createSetHandler)
+	handler.ServeHTTP(rr, req)
+	if status := rr.Code; status != http.StatusCreated {
+		t.Errorf("handler returned wrong status code: got %v want %v", status, http.StatusOK)
+	}
+
+	expected := `{"id":"setId"}`
+	if rr.Body.String() != expected {
+		t.Errorf("handler returned unexpected body: got %v want %v", rr.Body.String(), expected)
+	}
+
+	serviceMock.AssertExpectations(t)
+}
+
+func TestCreateSetHandlerErr(t *testing.T) {
+	userId := "userId"
+	workoutId := "workoutId"
+	exerciseId := "exerciseId"
+
+	reqBodyObj := createSetRequest{
+		Repetitions: 10,
+		Weight:      100,
+	}
+
+	reqBody, err := json.Marshal(reqBodyObj)
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	req, err := http.NewRequest("POST", "/workouts/"+workoutId+"/exercises/"+exerciseId+"/sets", bytes.NewBuffer(reqBody))
+	req.SetPathValue("id", workoutId)
+	req.SetPathValue("exerciseId", exerciseId)
+
+	req = populateContextWithSub(req, userId)
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	serviceMock := serviceMock{}
+	serviceMock.On("CreateAndReturnId", req.Context(), mock.MatchedBy(func(input createSetRequest) bool {
+		return input.Repetitions == 10 && input.Weight == 100
+	}), exerciseId, userId).
+		Return("", errors.New("Failed")).
+		Once()
+
+	rr := httptest.NewRecorder()
+	s := handler{service: &serviceMock}
+	handler := http.HandlerFunc(s.createSetHandler)
+	handler.ServeHTTP(rr, req)
+	if status := rr.Code; status != http.StatusBadRequest {
+		t.Errorf("handler returned wrong status code: got %v want %v", status, http.StatusOK)
+	}
+
+	expected := "Failed to create set\n"
 	if rr.Body.String() != expected {
 		t.Errorf("handler returned unexpected body: got %v want %v", rr.Body.String(), expected)
 	}
