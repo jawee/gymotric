3557f24bd67c509d4cb09a047d0815870857bc61
Andreas Olsson <olsson.andreas@gmail.com>
2025-06-06 15:21:31 +0200

    Minor adjustments

--- frontend/src/components/menu.tsx
+++ frontend/src/components/menu.tsx
@@ -9,9 +9,11 @@ const Menu = () => {
   return (
     <div className="flex mb-4">
       <NavigationMenu className="w-full justify-evenly max-w-none text-center md:max-w-xs">
-        <div className="flex h-10 w-14 items-center justify-center rounded-none text-primary m-1 ml-0 border-r-2 border-primary">
-          <Dumbbell className="size-6" />
-        </div>
+        <a href="/app">
+          <div className="flex h-10 w-14 items-center justify-center rounded-none text-primary m-1 ml-0 border-r-2 border-primary">
+            <Dumbbell className="size-6" />
+          </div>
+        </a>
         <NavigationMenuLink className="border-r-2 rounded-none p-2 grow border-primary" asChild><Link to="/app">Home</Link></NavigationMenuLink>
         <NavigationMenuLink className="border-r-2 rounded-none p-2 grow border-primary" asChild><Link to="/app/exercise-types">Exercises</Link></NavigationMenuLink>
         <NavigationMenuLink className="rounded-none p-2 grow" asChild><Link to="/app/profile">Profile</Link></NavigationMenuLink>
--- frontend/src/routes/home.tsx
+++ frontend/src/routes/home.tsx
@@ -4,9 +4,9 @@ import WorkoutsList from '../components/workouts-list';
 const Home = () => {
   return (
     <>
-      <h2 className="text-xl mb-2">Statistics</h2>
+      <h1 className="text-xl mb-2">Welcome</h1>
       <Statistics showWeekly={true} showMonthly={false} showYearly={false} />
-      <h2 className="text-xl mt-2 mb-2">Workouts</h2>
+      <h2 className="text-xl mt-4 mb-2">Workouts</h2>
       <WorkoutsList />
     </>
   );
